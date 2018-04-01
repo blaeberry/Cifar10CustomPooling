09dac527de88be6f4039f1eed3deaa2e8fbeb860
Evan Fuchs <efuchs@live.com>
2018-04-01 04:50:20 -0500

    add baseline to check impl

--- pytorch/wide-resnet.pytorch-master/networks/__init__.py
+++ pytorch/wide-resnet.pytorch-master/networks/__init__.py
@@ -2,6 +2,7 @@ from .lenet import *
 from .vggnet import *
 from .resnet import *
 from .wide_resnet import *
+from .wide_resnet_2d_normal import *
 from .wide_resnet_2d import *
 from .wide_resnet_2d_const import *
 from .wide_resnet_2d_resize_norm import *
