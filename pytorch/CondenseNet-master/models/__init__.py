7bf1678cf4150d89fb48539bc784a7dfaf0f9e73
Evan Fuchs <efuchs@live.com>
2018-04-06 00:54:21 -0500

    add 2d condensenet

--- pytorch/CondenseNet-master/models/__init__.py
+++ pytorch/CondenseNet-master/models/__init__.py
@@ -1,4 +1,5 @@
 from .densenet import DenseNet as densenet
 from .condensenet import CondenseNet as condensenet
+from .condensenet2d import CondenseNet2d as condensenet2d
 from .condensenet_converted import CondenseNet as condensenet_converted
 from .densenet_LGC import DenseNet_LGC as densenet_LGC
