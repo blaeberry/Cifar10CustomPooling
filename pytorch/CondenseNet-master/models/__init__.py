1a033f099701270e0246d072c0e352a4af997cf4
Evan Fuchs <efuchs@live.com>
2018-04-06 20:44:06 -0500

    forgot init

--- pytorch/CondenseNet-master/models/__init__.py
+++ pytorch/CondenseNet-master/models/__init__.py
@@ -1,5 +1,6 @@
 from .densenet import DenseNet as densenet
 from .condensenet import CondenseNet as condensenet
 from .condensenet2d import CondenseNet2d as condensenet2d
+from .condensenetdw import CondenseNetDW as condensenetdw
 from .condensenet_converted import CondenseNet as condensenet_converted
 from .densenet_LGC import DenseNet_LGC as densenet_LGC
