bbcf11a2f0202fe91e8257aa8a06250a4439d892
Evan Fuchs <efuchs@live.com>
2018-04-09 02:54:02 -0500

    add slower re1d

--- pytorch/wide-resnet.pytorch-master/main.py
+++ pytorch/wide-resnet.pytorch-master/main.py
@@ -114,6 +114,10 @@ def getNetwork(args):
         net = Wide_ResNet_RE1D(args.depth, args.widen_factor, args.dropout, num_classes, 32, 32)
         file_name = 'wide-resnet-re1d-'+str(args.depth)+'x'+str(args.widen_factor) + \
                     '-drop-'+str(args.dropout)+'-wd-'+str(args.wd)+'-nest-'+str(args.nesterov)
+    elif (args.net_type == 'wide-resnet-re1d-slow'):
+        net = Wide_ResNet_RE1D_Slow(args.depth, args.widen_factor, args.dropout, num_classes, 32, 32)
+        file_name = 'wide-resnet-re1d-slow-'+str(args.depth)+'x'+str(args.widen_factor) + \
+                    '-drop-'+str(args.dropout)+'-wd-'+str(args.wd)+'-nest-'+str(args.nesterov)
     elif (args.net_type == 'wide-resnet-1d-resize-avg'):
         net = Wide_ResNet_1D_Resize_Avg(args.depth, args.widen_factor, args.dropout, num_classes, 32, 32)
         file_name = 'wide-resnet-1d-resize-avg-'+str(args.depth)+'x'+str(args.widen_factor) + \
