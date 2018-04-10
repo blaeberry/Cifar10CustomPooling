d8ca846296592fe62b8a99a37923276d7a06aa0f
Evan Fuchs <efuchs@live.com>
2018-04-10 02:37:27 -0500

    add densenet pref pool

--- pytorch/CondenseNet-master/models/__init__.py
+++ pytorch/CondenseNet-master/models/__init__.py
@@ -1,5 +1,6 @@
 from .densenet import DenseNet as densenet
 from .densenetre1d import DenseNetRe1d as densenetre1d
+from .densenet-ppb import DenseNetPPB as densenet-ppb
 from .condensenet import CondenseNet as condensenet
 from .condensenet2d import CondenseNet2d as condensenet2d
 from .condensenetdw import CondenseNetDW as condensenetdw
