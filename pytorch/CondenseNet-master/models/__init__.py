cf29797e8141aa72a753252f4bf27c5888cd3339
fuchs <fuchs@live.com>
2018-04-10 03:27:16 -0500

    densenet ppb

--- pytorch/CondenseNet-master/models/__init__.py
+++ pytorch/CondenseNet-master/models/__init__.py
@@ -1,6 +1,6 @@
 from .densenet import DenseNet as densenet
 from .densenetre1d import DenseNetRe1d as densenetre1d
-from .densenet-ppb import DenseNetPPB as densenet-ppb
+from .densenet_ppb import DenseNetPPB as densenet_ppb
 from .condensenet import CondenseNet as condensenet
 from .condensenet2d import CondenseNet2d as condensenet2d
 from .condensenetdw import CondenseNetDW as condensenetdw
